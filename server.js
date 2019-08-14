c7f412c0000cd24cc492708de7352eac5fc99afb
moaz azam <moazazam4356@gmail.com>
2019-08-13 20:21:34 -0700

    K

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get("/blogs",  function(req, res){
         if(err){
             console.log(err);
         }else{
-            res.render("index", {blogs: blogs});
+            res.render("blogs", {blogs: blogs});
         }
     });
 });
